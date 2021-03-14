db6c5d530fe7a410d1102734c73cb5823196b3d9
malek2005 <malek2005@list.ru>
2021-03-14 16:13:45 +0200

    feat: solutin for towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,30 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+   
+  let matrix1 = [];
+  let valueReverse;
+  if (!matrix) {
+    return [];
+  }  
+  for (const [key, value] of Object.entries(matrix)){
+  if (key % 2 == 0)
+  {
+    for (const [subkey,subvalue] of Object.entries(value))
+  {
+    matrix1.push(subvalue);
+  }  
+  } 
+
+    else  {
+    
+      valueReverse = value.reverse();
+      for (const [subkey, subvalue] of Object.entries(valueReverse))
+      {
+        matrix1.push (subvalue);
+      }
+  } 
+  
+}
+return matrix1;
 }
